fba68d18f2e9eb7e35f7683d9b6b72b4f9a74a52
timo4k <tedmondc513@gmail.com>
2021-10-21 20:47:12 -0400

    first commit

---
+++ script.py
@@ -0,0 +1,31 @@
+# def bubble_sort(data):
+#     swapped = True
+#     while swapped:
+#         swapped = False
+#         for i in range(len(data)-1):
+#             if data[i] > data[i+1]:
+#                 data[i], data[i+1] = data[i+1], data[i]
+#                 swapped = True
+    
+# if __name__ == '__main__':
+#     data = [9, 5, 13, 62, 20, 190, 77, 924, 104, 2304]
+#     bubble_sort(data)
+#     print(data)
+
+# for x in range(1, 101):
+#     i = ''
+#     if x % 3 == 0:
+#         i = 'fizz'
+#     if x % 5 == 0:
+#         i += 'buzz'
+#     print(x if not i else i)
+
+for i in range(1, 101):
+    if i % 5 and i % 3 == 0:
+        print('fizz buzz')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
